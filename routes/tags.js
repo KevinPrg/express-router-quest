bdd7948b00c00d81600df7ba563c41762f67456b
psycho33200 <pininfarina47@gmail.com>
2019-12-29 10:29:37 +0100

    terminate with bonus

--- routes/tags.js
+++ routes/tags.js
@@ -1,9 +1,17 @@
-const fakeTags = require("../data/tags");
 const express = require("express");
-const router = express.Router();
+
+const router = express.Router({ mergeParams: true });
+const fakeTags = require("../data/tags");
+const fakePosts = require("../data/posts");
 
 router.get("/", (req, res) => {
   res.json(fakeTags);
+});
+
+router.get("/:tagId/posts", (req, res) => {
+  const tagId = Number(req.params.tagId);
+  const tags = fakePosts.filter(tag => tag.tag_id.includes(tagId));
+  res.json(tags);
 });
 
 module.exports = router;
